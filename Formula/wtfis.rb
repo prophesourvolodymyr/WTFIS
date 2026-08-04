a84556e17265114ea72826885006e8dd52a5057d
volodymur <scinceofdescepline23@gmail.com>
2026-08-04 07:04:47 -0700

    Fix Homebrew Cargo install path

--- Formula/wtfis.rb
+++ Formula/wtfis.rb
@@ -8,7 +8,7 @@ class Wtfis < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args(path: "Cargo.toml")
+    system "cargo", "install", *std_cargo_args(path: ".")
     bin.install_symlink "wtfis" => "cdd"
     pkgshare.install "shell/wtfis.zsh", "shell/wtfis.bash", "shell/wtfis.ps1"
   end
@@ -28,6 +28,6 @@ class Wtfis < Formula
   end
 
   test do
-    assert_match "wtfis", shell_output("#{bin}/wtfis --help 2>&1", 0)
+    assert_match "wtfis", shell_output("#{bin}/wtfis --help 2>&1")
   end
 end
